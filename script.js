633914d0dae5134a7e0cbd01963418c1e0508efe
dejourford <dejourford@my.unt.edu>
2023-02-11 17:00:22 -0600

    add alert, header, and hero section

---
+++ script.js
@@ -0,0 +1,11 @@
+// == FETCH API == //
+const url = 'https://fakestoreapi.com/products/'
+fetch(url)
+.then(response => response.json())
+.then (data => {
+    console.log(data)
+})
+.catch(error => {
+    console.log(`error ${error}`)
+})
+
