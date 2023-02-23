235be63e60d13948a38213ae3c39735751ea7f29
De'Jour Ford <dejourford@my.unt.edu>
2023-02-23 00:42:55 -0600

    add id tags to images and define variables

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // == DEFINE VARIABLES == //
+
+// when these are clicked, details modal will show
 const featureOne = document.getElementById('feature-one')
 const featureTwo = document.getElementById('feature-two')
 const featureThree = document.getElementById('feature-three')
 const featureFour = document.getElementById('feature-four')
 
+// these are for dom manipulation
+const featureOneImg = document.getElementById('feature-one-image')
+const featureTwoImg = document.getElementById('feature-two-image')
+const featureThreeImg = document.getElementById('feature-three-image')
+const featureFourImg = document.getElementById('feature-four-image')
 
 
 
@@ -15,6 +22,7 @@ async function getData() {
       const response = await fetch('https://fakestoreapi.com/products/');
       const data = await response.json();
       console.log(data);
+        featureOneImg.src = data[0].image
     } catch (error) {
       console.error(error);
     }
