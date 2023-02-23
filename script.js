1c9f83187dd60f0318e5f751fd81a83af4cd2524
De'Jour Ford <dejourford@my.unt.edu>
2023-02-23 01:03:10 -0600

    add price id tags and add text content to features

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ const featureTwo = document.getElementById('feature-two')
 const featureThree = document.getElementById('feature-three')
 const featureFour = document.getElementById('feature-four')
 
-// these are for dom manipulation
+// THESE ARE FOR DOM MANIPULATION
+// --images
 const featureOneImg = document.getElementById('feature-one-image')
 const featureTwoImg = document.getElementById('feature-two-image')
 const featureThreeImg = document.getElementById('feature-three-image')
 const featureFourImg = document.getElementById('feature-four-image')
 
+// --product title
+const featureOneTitle = document.getElementById('feature-one-title')
+const featureTwoTitle = document.getElementById('feature-two-title')
+const featureThreeTitle = document.getElementById('feature-three-title')
+const featureFourTitle = document.getElementById('feature-four-title')
+
+// --product prices
+const featureOnePrice = document.getElementById('feature-one-price')
+const featureTwoPrice = document.getElementById('feature-two-price')
+const featureThreePrice = document.getElementById('feature-three-price')
+const featureFourPrice = document.getElementById('feature-four-price')
 
 
 
@@ -22,10 +34,23 @@ async function getData() {
       const response = await fetch('https://fakestoreapi.com/products/');
       const data = await response.json();
       console.log(data);
+        // --images
         featureOneImg.src = data[0].image
         featureTwoImg.src = data[1].image
         featureThreeImg.src = data[2].image
         featureFourImg.src = data[3].image
+
+        // --titles
+        featureOneTitle.textContent = data[0].title
+        featureTwoTitle.textContent = data[1].title
+        featureThreeTitle.textContent = data[2].title
+        featureFourTitle.textContent = data[3].title
+
+        // --prices
+        featureOnePrice.textContent = '$' + data[0].price
+        featureTwoPrice.textContent = '$' + data[1].price
+        featureThreePrice.textContent = '$' + data[2].price
+        featureFourPrice.textContent = '$' + data[3].price
     } catch (error) {
       console.error(error);
     }
